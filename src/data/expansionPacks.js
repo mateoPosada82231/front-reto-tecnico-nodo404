be9bb819adce648cc880c54eb33f44a5b2d1de38
nathalystevenson <jstevenson@unal.edu.co>
2026-07-19 19:58:08 -0500

    Renames ExtensionsPack and PacksMedia

--- src/data/expansionPacks.js
+++ src/data/expansionPacks.js
@@ -3,92 +3,92 @@ import { packMedia } from './packMedia.js';
 export const expansionPacks = [
   {
     id: "featured-1",
-    nombre: "Los Sims 4: Vida en el Rancho",
-    categoria: "Expansion Pack",
-    precio: 159900,
-    descripcion: "Crea tu propia vida rústica. Cría y entrena caballos, entabla amistad con mini-cabras y mini-ovejas, y elabora tu propio néctar casero para vender o compartir.",
-    imagen: packMedia[1].imagen,
+    name: "Los Sims 4: Vida en el Rancho",
+    category: "Expansion Pack",
+    price: 159900,
+    description: "Crea tu propia vida rústica. Cría y entrena caballos, entabla amistad con mini-cabras y mini-ovejas, y elabora tu propio néctar casero para vender o compartir.",
+    image: packMedia[1].image,
     featured: true,
-    linkTienda: packMedia[1].linkTienda
+    link: packMedia[1].storeLink
   },
   {
     id: "pack-1",
-    nombre: "Los Sims 4: Días de Universidad",
-    categoria: "Expansion Pack",
-    precio: 159900,
-    descripcion: "Inscríbete en la universidad, explora experiencias académicas y asiste a actividades extracurriculares.",
-    imagen: packMedia[2].imagen,
+    name: "Los Sims 4: Días de Universidad",
+    category: "Expansion Pack",
+    price: 159900,
+    description: "Inscríbete en la universidad, explora experiencias académicas y asiste a actividades extracurriculares.",
+    image: packMedia[2].image,
     featured: false,
-    linkTienda: packMedia[2].linkTienda
+    link: packMedia[2].storeLink
   },
   {
     id: "pack-2",
-    nombre: "Los Sims 4: Urbanitas",
-    categoria: "Expansion Pack",
-    precio: 159900,
-    descripcion: "Lleva a tus Sims a la gran ciudad y experimenta todo lo que ofrece, desde el bullicio exterior hasta situaciones exclusivas de los apartamentos.",
-    imagen: packMedia[3].imagen,
+    name: "Los Sims 4: Urbanitas",
+    category: "Expansion Pack",
+    price: 159900,
+    description: "Lleva a tus Sims a la gran ciudad y experimenta todo lo que ofrece, desde el bullicio exterior hasta situaciones exclusivas de los apartamentos.",
+    image: packMedia[3].image,
     featured: false,
-    linkTienda: packMedia[3].linkTienda
+    link: packMedia[3].storeLink
   },
   {
     id: "pack-3",
-    nombre: "Los Sims 4: Y Las Cuatro Estaciones",
-    categoria: "Expansion Pack",
-    precio: 159900,
-    descripcion: "Añade el clima a la vida de tus Sims para contar nuevas historias, disfrutar de actividades estacionales y celebrar festividades.",
-    imagen: packMedia[4].imagen,
+    name: "Los Sims 4: Y Las Cuatro Estaciones",
+    category: "Expansion Pack",
+    price: 159900,
+    description: "Añade el clima a la vida de tus Sims para contar nuevas historias, disfrutar de actividades estacionales y celebrar festividades.",
+    image: packMedia[4].image,
     featured: false,
-    linkTienda: packMedia[4].linkTienda
+    link: packMedia[4].storeLink
   },
   {
     id: "pack-4",
-    nombre: "Los Sims 4: ¡A Trabajar!",
-    categoria: "Expansion Pack",
-    precio: 159900,
-    descripcion: "Controla activamente a tus Sims en sus lugares de trabajo y determina si se dirigen hacia un gran ascenso o se convierten en una amenaza.",
-    imagen: packMedia[5].imagen,
+    name: "Los Sims 4: ¡A Trabajar!",
+    category: "Expansion Pack",
+    price: 159900,
+    description: "Controla activamente a tus Sims en sus lugares de trabajo y determina si se dirigen hacia un gran ascenso o se convierten en una amenaza.",
+    image: packMedia[5].image,
     featured: false,
-    linkTienda: packMedia[5].linkTienda
+    link: packMedia[5].storeLink
   },
   {
     id: "pack-5",
-    nombre: "Los Sims 4: Perros y Gatos",
-    categoria: "Expansion Pack",
-    precio: 159900,
-    descripcion: "Crea una variedad de perros y gatos, añádelos a los hogares de tus Sims para cambiar sus vidas para siempre y cuida de las mascotas del vecindario.",
-    imagen: packMedia[6].imagen,
+    name: "Los Sims 4: Perros y Gatos",
+    category: "Expansion Pack",
+    price: 159900,
+    description: "Crea una variedad de perros y gatos, añádelos a los hogares de tus Sims para cambiar sus vidas para siempre y cuida de las mascotas del vecindario.",
+    image: packMedia[6].image,
     featured: false,
-    linkTienda: packMedia[6].linkTienda
+    link: packMedia[6].storeLink
   },
   {
     id: "pack-6",
-    nombre: "Los Sims 4: Vida Isleña",
-    categoria: "Expansion Pack",
-    precio: 159900,
-    descripcion: "Escapa de la rutina y disfruta de un estilo de vida relajado rodeado de playas en Sulani, donde el sol brilla con fuerza y las noches son refrescantes.",
-    imagen: packMedia[7].imagen,
+    name: "Los Sims 4: Vida Isleña",
+    category: "Expansion Pack",
+    price: 159900,
+    description: "Escapa de la rutina y disfruta de un estilo de vida relajado rodeado de playas en Sulani, donde el sol brilla con fuerza y las noches son refrescantes.",
+    image: packMedia[7].image,
     featured: false,
-    linkTienda: packMedia[7].linkTienda
+    link: packMedia[7].storeLink
   },
   {
     id: "pack-7",
-    nombre: "Los Sims 4: Escapada en la Nieve",
-    categoria: "Expansion Pack",
-    precio: 159900,
-    descripcion: "Equípate para los deportes de invierno, relájate en aguas termales y diseña tu hogar de estilo japonés.",
-    imagen: packMedia[8].imagen,
+    name: "Los Sims 4: Escapada en la Nieve",
+    category: "Expansion Pack",
+    price: 159900,
+    description: "Equípate para los deportes de invierno, relájate en aguas termales y diseña tu hogar de estilo japonés.",
+    image: packMedia[8].image,
     featured: false,
-    linkTienda: packMedia[8].linkTienda
+    link: packMedia[8].storeLink
   },
   {
     id: "pack-8",
-    nombre: "Los Sims 4: Se Alquila",
-    categoria: "Expansion Pack",
-    precio: 159900,
-    descripcion: "Experimenta la vida en vecindarios multifamiliares como inquilino o propietario de propiedades residenciales en alquiler.",
-    imagen: packMedia[9].imagen,
+    name: "Los Sims 4: Se Alquila",
+    category: "Expansion Pack",
+    price: 159900,
+    description: "Experimenta la vida en vecindarios multifamiliares como inquilino o propietario de propiedades residenciales en alquiler.",
+    image: packMedia[9].image,
     featured: false,
-    linkTienda: packMedia[9].linkTienda
+    link: packMedia[9].storeLink
   }
 ];
