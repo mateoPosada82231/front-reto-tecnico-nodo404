c67be665f3a8d2c357d88125c79a288384b4dd3b
Dayana Marcela Vallejo Ortega <vallejo9605@gmail.com>
2026-07-20 08:50:22 -0500

    feat: add welcome modal and improve expansion cards

--- src/features/landing/pages/LandingPage.jsx
+++ src/features/landing/pages/LandingPage.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import HeroSection from './HeroSection';
 import ExpansionGrid from './ExpansionGrid';
+import WelcomeModal from '../../../shared/components/molecules/WelcomeModal';
 
 function LandingPage() {
   return (
     <div className="w-full flex flex-col gap-12 pb-16">
+
+      <WelcomeModal />
+
       <HeroSection />
+
       <ExpansionGrid />
+
     </div>
   );
 }
